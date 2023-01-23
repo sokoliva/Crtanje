4523043c5449f06dd601750170cd7ed2e7e947ad
sokoliva <sokoliva.math@pmf.hr>
2023-01-23 16:38:37 +0100

    Project added

---
+++ Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿using System.Windows.Forms;
+
+namespace Crtanje
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripDropDownButton1 = new System.Windows.Forms.ToolStripDropDownButton();
+            this.vizualnoUredivanjeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.kodToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.obojeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.BackColor = System.Drawing.SystemColors.Control;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripLabel1,
+            this.toolStripDropDownButton1,
+            this.toolStripButton1,
+            this.toolStripButton2});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(800, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(41, 22);
+            this.toolStripLabel1.Text = "Prikaz:";
+            // 
+            // toolStripDropDownButton1
+            // 
+            this.toolStripDropDownButton1.AutoSize = false;
+            this.toolStripDropDownButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.None;
+            this.toolStripDropDownButton1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.vizualnoUredivanjeToolStripMenuItem,
+            this.kodToolStripMenuItem,
+            this.obojeToolStripMenuItem});
+            this.toolStripDropDownButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripDropDownButton1.Name = "toolStripDropDownButton1";
+            this.toolStripDropDownButton1.Size = new System.Drawing.Size(50, 22);
+            this.toolStripDropDownButton1.Text = "V";
+            this.toolStripDropDownButton1.ToolTipText = "Odaberi prikaz\r\n";
+            // 
+            // vizualnoUredivanjeToolStripMenuItem
+            // 
+            this.vizualnoUredivanjeToolStripMenuItem.Name = "vizualnoUredivanjeToolStripMenuItem";
+            this.vizualnoUredivanjeToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.vizualnoUredivanjeToolStripMenuItem.Text = "Vizualno uređivanje";
+            this.vizualnoUredivanjeToolStripMenuItem.Click += new System.EventHandler(this.vizualnoUredivanjeToolStripMenuItem_Click);
+            // 
+            // kodToolStripMenuItem
+            // 
+            this.kodToolStripMenuItem.Name = "kodToolStripMenuItem";
+            this.kodToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.kodToolStripMenuItem.Text = "Kod";
+            this.kodToolStripMenuItem.Click += new System.EventHandler(this.kodToolStripMenuItem_Click);
+            // 
+            // obojeToolStripMenuItem
+            // 
+            this.obojeToolStripMenuItem.Name = "obojeToolStripMenuItem";
+            this.obojeToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.obojeToolStripMenuItem.Text = "Oboje";
+            this.obojeToolStripMenuItem.Click += new System.EventHandler(this.obojeToolStripMenuItem_Click);
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(33, 22);
+            this.toolStripButton1.Text = "Crta";
+            this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(36, 22);
+            this.toolStripButton2.Text = "Krug";
+            this.toolStripButton2.Click += new System.EventHandler(this.toolStripButton2_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.richTextBox1.Location = new System.Drawing.Point(0, 28);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.ForcedVertical;
+            this.richTextBox1.ShowSelectionMargin = true;
+            this.richTextBox1.Size = new System.Drawing.Size(187, 426);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Form1";
+            this.Text = "Crtanje";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.ToolStripDropDownButton toolStripDropDownButton1;
+        private System.Windows.Forms.ToolStripMenuItem vizualnoUredivanjeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem kodToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem obojeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using static System.Net.Mime.MediaTypeNames;
+using System.Text.RegularExpressions;
+
+
+
+namespace Crtanje
+{
+    public partial class Form1 : Form
+    {
+        private Point PrvaTocka = new Point(-1, -1);
+        private Point DrugaTocka = new Point(-1, -1);
+        private int odabir = 0; //0 nista nije odabrano, 1 odabrana je crta, 2 odabran je krug
+      
+        List<PairPoints> SveCrte= new List<PairPoints>();
+        List<Rectangle> SviKrugovi = new List<Rectangle>();
+       
+        List<PairPoints> TextSveCrte = new List<PairPoints>();
+        List<Rectangle> TextSviKrugovi = new List<Rectangle>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            richTextBox1.Visible = false;
+            toolStripButton1.Visible = false;
+            toolStripButton2.Visible = false;   
+        }
+
+        private void obojeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Visible = true;
+            toolStripButton1.Visible = true;
+            toolStripButton2.Visible = true;
+        }
+
+        private void kodToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Visible = true;
+            toolStripButton1.Visible = false;
+            toolStripButton2.Visible = false;
+            odabir = 0;
+
+        }
+
+        private void vizualnoUredivanjeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Visible = false;
+            toolStripButton1.Visible = true;
+            toolStripButton2.Visible = true;
+        }
+        
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            odabir = 1;
+        }
+
+        private void toolStripButton2_Click(object sender, EventArgs e)
+        {
+            odabir = 2;
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (odabir != 0)
+            {
+                if (PrvaTocka.Equals(new Point(-1, -1)))
+                {
+                    PrvaTocka = new Point(e.X, e.Y);
+                }
+
+                else
+                {
+                    DrugaTocka = new Point(e.X, e.Y);
+                    var g = CreateGraphics();
+                    g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+
+                    if (odabir == 1)
+                    {
+                        g.DrawLine(Pens.IndianRed, PrvaTocka, DrugaTocka);
+                        PairPoints nova = new PairPoints(PrvaTocka, DrugaTocka);
+                        SveCrte.Add(nova);
+                    }
+                    else
+                    {
+                        double radius = Math.Sqrt((PrvaTocka.X - e.X) * (PrvaTocka.X - e.X) + (PrvaTocka.Y - e.Y) * (PrvaTocka.Y - e.Y));
+                        Rectangle rect = new Rectangle(PrvaTocka.X - (int)radius, PrvaTocka.Y - (int)radius, 2 * (int)radius, 2 * (int)radius);
+                        g.DrawEllipse(Pens.IndianRed, rect);
+                        SviKrugovi.Add(rect);
+                    }
+
+                    PrvaTocka = new Point(-1, -1);
+                }
+            }
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            int x1 = 1, y1 = 1, x2 = 1, y2 = 1, r = 1;
+            TextSveCrte.Clear();
+            TextSviKrugovi.Clear();
+
+            var g = CreateGraphics();
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+            this.Invalidate();
+
+            Regex rgCrta = new Regex(@"Crta\s*\(\s*[0-9]+\s*,\s*[0-9]+\s*,\s*[0-9]+\s*,\s*[0-9]+\s*\)\s*", RegexOptions.IgnoreCase);
+            Regex rgKrug = new Regex(@"Krug\s*\(\s*[0-9]+\s*,\s*[0-9]+\s*,\s*[0-9]+\s*\)\s*", RegexOptions.IgnoreCase);
+            //iteracija kroz linije
+            foreach (string str in richTextBox1.Lines)
+            {
+                if (rgCrta.IsMatch(str))
+                {
+                    string[] brojke = Regex.Split(str, @"\D+");
+                    
+                    int.TryParse(brojke[1], out x1);
+                    int.TryParse(brojke[2], out y1);
+                    int.TryParse(brojke[3], out x2);
+                    int.TryParse(brojke[4], out y2);
+
+                    Point prva_ = new Point(x1, y1);
+                    Point druga_ = new Point(x2, y2);
+                    
+                    g.DrawLine(Pens.IndianRed, prva_, druga_);
+                    PairPoints prva_i_druga = new PairPoints(prva_, druga_);
+                    if (!(TextSveCrte.Contains(prva_i_druga))) TextSveCrte.Add(prva_i_druga);
+                }
+
+                if (rgKrug.IsMatch(str))
+                {
+                    string[] brojke = Regex.Split(str, @"\D+");
+
+                    int.TryParse(brojke[1], out x1);
+                    int.TryParse(brojke[2], out y1);
+                    int.TryParse(brojke[3], out r);
+
+                    Rectangle rect = new Rectangle(x1, y1, r*2, r*2);
+
+                    g.DrawEllipse(Pens.IndianRed, rect);
+                    if (!(TextSviKrugovi.Contains(rect))) TextSviKrugovi.Add(rect);
+                }
+            }
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            foreach (PairPoints crta in SveCrte)
+            {
+                e.Graphics.DrawLine(Pens.IndianRed, crta.prva, crta.druga);
+            }
+            foreach(Rectangle krug in SviKrugovi)
+            {
+                e.Graphics.DrawEllipse(Pens.IndianRed, krug);
+            }
+            foreach (PairPoints crta in TextSveCrte)
+            {
+                e.Graphics.DrawLine(Pens.IndianRed, crta.prva, crta.druga);
+            }
+            foreach (Rectangle krug in TextSviKrugovi)
+            {
+                e.Graphics.DrawEllipse(Pens.IndianRed, krug);
+            }
+        }
+    }
+}
+
+class PairPoints
+{
+    public Point prva;
+    public Point druga;
+    public PairPoints(Point prva, Point druga)
+    {
+        this.prva = prva;
+        this.druga = druga;
+    }
+}
